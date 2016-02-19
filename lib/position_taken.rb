72b199daafd46bb2f8ab0323591b1c391a59f17e
Jamaleh <Jamaleh@hotmail.com>
2016-02-19 16:37:59 +1100

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,20 @@
-# code your #position_taken? method here!
+#code your #position_taken? method here!
+
+
+# def position_taken?(board, position)
+#   board[position] == "" || board[position] == " " || board[position] == nil ? false : true
+# end 
+
+
+
+def position_taken?(board, position)
+  if board[position] == ""
+    return false
+  elsif board[position] == " "
+    return false 
+  elsif board[position] == nil
+    return false
+  else 
+    return true
+  end 
+end 
